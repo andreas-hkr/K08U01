0d24b2667b29f65464835c4f870a002f3c931d41
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 14:16:19 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
+    static int sign(int number) {
+        if (number < 0) {
+            return -1;
+        } else if (number == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
     public static void main(String[] args) {
         // Skriv en metod som tar ett heltal som parameter och returnerar
         //   -1  om parametern är negativt
         //    0  om parametern är 0
         //    1  om parametern är större än 0
 
-
+        int result = sign(-5);
+        System.out.println("Metoden returnerade " + result);
     }
 }
