97e7f76478410dff691d6cc72129ea8eed3a2c01
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 13:51:06 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skriv en metod som tar ett heltal som parameter och returnerar
+        //   -1  om parametern är negativt
+        //    0  om parametern är 0
+        //    1  om parametern är 1
+
+
+    }
+}
